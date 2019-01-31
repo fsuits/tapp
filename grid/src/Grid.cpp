9fbeeca253a00e0ef541c6141d669d014c2b63ec
FrankSuits <frankst@au1.ibm.com>
2019-01-31 15:08:05 +1100

    added license header and removed README files

--- grid/src/Grid.cpp
+++ grid/src/Grid.cpp
@@ -1,3 +1,8 @@
+// Copyright 2019, IBM Corporation
+// 
+// This source code is licensed under the Apache License, Version 2.0 found in
+// the LICENSE.md file in the root directory of this source tree.
+
 #include <iostream>
 
 #include "LCMSFile/LCMSFile.h"
